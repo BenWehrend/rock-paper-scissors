e67e3e29536c7b8410850c3334ef68aeb585e93c
Ben Wehrend <benwehrend@gmail.com>
2024-03-03 12:18:51 -0800

    adds functional RPS classic and challenge

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var human = document.querySelector('.human');
+var player = document.querySelector('.player');
 var computer = document.querySelector('.computer');
 var classicGameButton = document.querySelector('.classic-game');
 var challengeGameButton = document.querySelector('.challenge-game');
@@ -8,9 +8,13 @@ var scissorsButton = document.querySelector('.scissors');
 var lizardButton = document.querySelector('.lizard');
 var alienButton = document.querySelector('.alien');
 var changeGameButton = document.querySelector('.change-game');
+var title = document.querySelector('.title');
 var subtitle = document.querySelector('.subtitle');
 
-function createPlayer(name, token, wins = 0) {
+var classicArray = ['rock', 'paper', 'scissors'];
+var challengeArray = ['rock', 'paper', 'scissors', 'lizard', 'alien'];
+
+function createPlayer(name, token, wins) {
     return {
         name,
         token,
@@ -18,15 +22,33 @@ function createPlayer(name, token, wins = 0) {
     }
 }
 
-function takeTurn(player1, player2) {
-
+function addClassicEvents() {
+    rockButton.addEventListener('click', rockChoice);
+    paperButton.addEventListener('click', paperChoice);
+    scissorsButton.addEventListener('click', scissorsChoice);
 }
 
-function createGame(player1, player2) {
+function removeClassicEvents() {
+    rockButton.removeEventListener('click', rockChoice);
+    paperButton.removeEventListener('click', paperChoice);
+    scissorsButton.removeEventListener('click', scissorsChoice);
+}
 
+function addChallengeEvents() {
+    addClassicEvents();
+    lizardButton.addEventListener('click', lizardChoice);
+    alienButton.addEventListener('click', alienChoice);
+}
+
+function removeChallengeEvents() {
+    removeClassicEvents();
+    lizardButton.removeEventListener('click', lizardChoice);
+    alienButton.removeEventListener('click', alienChoice);
 }
 
 classicGameButton.addEventListener('click', startClassicGame);
+challengeGameButton.addEventListener('click', startChallengeGame);
+changeGameButton.addEventListener('click', changeGame);
 
 function startClassicGame() {
     classicGameButton.style.visibility = "hidden";
@@ -36,9 +58,8 @@ function startClassicGame() {
     scissorsButton.style.visibility = "visible";
     changeGameButton.style.visibility = "visible";
     subtitle.innerHTML = "Choose your fighter!";
+    addClassicEvents();
 }
-
-challengeGameButton.addEventListener('click', startChallengeGame);
 
 function startChallengeGame() {
     classicGameButton.style.visibility = "hidden";
@@ -49,10 +70,11 @@ function startChallengeGame() {
     lizardButton.style.visibility = "visible";
     alienButton.style.visibility = "visible";
     changeGameButton.style.visibility = "visible";
+    title.innerHTML = "Rock, Paper, Scissors, Lizard, Alien"
     subtitle.innerHTML = "Choose your fighter!";
+    addClassicEvents();
+    addChallengeEvents();
 }
-
-changeGameButton.addEventListener('click', changeGame);
 
 function changeGame() {
     classicGameButton.style.visibility = "visible";
@@ -63,5 +85,71 @@ function changeGame() {
     lizardButton.style.visibility = "hidden";
     alienButton.style.visibility = "hidden";
     changeGameButton.style.visibility = "hidden";
+    title.innerHTML = "Rock, Paper, Scissors";
     subtitle.innerHTML = "Choose your game!";
+    removeClassicEvents();
+    removeChallengeEvents();
+}
+
+function playClassicGame(playerChoice) {
+    var computerChoice = classicArray[Math.floor(Math.random() * classicArray.length)];
+    compareChoices(playerChoice, computerChoice);
+}
+
+function playChallengeGame(playerChoice) {
+    var computerChoice = challengeArray[Math.floor(Math.random() * challengeArray.length)];
+    compareChallengeChoices(playerChoice, computerChoice);
+}
+
+function compareChoices(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        subtitle.innerHTML = "It's a tie!";
+    } else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
+               (playerChoice === 'paper' && computerChoice === 'rock') ||
+               (playerChoice === 'scissors' && computerChoice === 'paper')) {
+        subtitle.innerHTML = "Congrats! You win!";
+    } else {
+        subtitle.innerHTML = "Oh no! You lose!";
+    }
+}
+
+function compareChallengeChoices(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        subtitle.innerHTML = "It's a tie!";
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper') ||
+        (playerChoice === 'rock' && computerChoice === 'lizard') ||
+        (playerChoice === 'lizard' && computerChoice === 'alien') ||
+        (playerChoice === 'alien' && computerChoice === 'scissors') ||
+        (playerChoice === 'scissors' && computerChoice === 'lizard') ||
+        (playerChoice === 'lizard' && computerChoice === 'paper') ||
+        (playerChoice === 'paper' && computerChoice === 'alien') ||
+        (playerChoice === 'alien' && computerChoice === 'rock')
+    ) {
+        subtitle.innerHTML = "Congrats! You win!";
+    } else {
+        subtitle.innerHTML = "Oh no! You lose!";
+    }
+}
+
+function rockChoice() {
+    playClassicGame('rock');
+}
+
+function paperChoice() {
+    playClassicGame('paper');
+}
+
+function scissorsChoice() {
+    playClassicGame('scissors');
+}
+
+function lizardChoice() {
+    playChallengeGame('lizard');
+}
+
+function alienChoice() {
+    playChallengeGame('alien');
 }
